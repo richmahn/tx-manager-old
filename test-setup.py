56ee2456f1f121a48ac7ea1c211db406bebfe254
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-20 15:24:56 -0600

    Updated dependencies

--- test-setup.py
+++ test-setup.py
@@ -12,11 +12,15 @@ setup(
     long_description='Unit test setup file',
     classifiers=[],
     dependency_links=[
-                'git+git://github.com/richmahn/tx-shared-tools.git@develop#egg=tx-shared-tools',
+        'git+git://github.com/richmahn/tx-shared-tools.git@develop#egg=tx-shared-tools',
+        'git+git://github.com/richmahn/tx-shared-tools.git@develop#egg=aws_tools',
+        'git+git://github.com/richmahn/tx-shared-tools.git@develop#egg=general_tools',
+        'git+git://github.com/richmahn/tx-shared-tools.git@develop#egg=gogs_tools',
     ],
     install_requires=[
         'requests',
-        'tx-shared-tools'
+        'aws_tools',
+        'gogs_tools'
     ],
     test_suite='tests'
 )
